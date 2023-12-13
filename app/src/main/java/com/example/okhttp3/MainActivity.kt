ee5c73584251828b70113db5962c9a9d9a2e16c1
MadziaSzmatloch <magdaszmatloch@gmail.com>
2023-12-13 14:19:26 +0100

    update

--- app/src/main/java/com/example/okhttp3/MainActivity.kt
+++ app/src/main/java/com/example/okhttp3/MainActivity.kt
@@ -15,48 +15,63 @@ import okhttp3.Response
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityText: TextView
+    private lateinit var participiantsText: TextView
+    private lateinit var typeText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val activitytext: TextView = findViewById(R.id.activity)
-        val participiantstext: TextView = findViewById(R.id.participians)
-        val typetext: TextView = findViewById(R.id.type)
+        activityText = findViewById(R.id.activity)
+        participiantsText = findViewById(R.id.participians)
+        typeText = findViewById(R.id.type)
         val loadBtn: Button = findViewById(R.id.LoadNewBtn)
+
         loadBtn.setOnClickListener{
-            val client = OkHttpClient()
-
-            val request = Request.Builder()
-                .url("https://www.boredapi.com/api/activity")
-                .build()
-
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    runOnUiThread {
-                        Toast.makeText(this@MainActivity, "Error fetching data", Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    val responseData = response.body?.string()
-                    if (!responseData.isNullOrBlank()) {
-                        val gson = Gson()
-                        val item = gson.fromJson(responseData, ActivityDto::class.java)
-
-                        runOnUiThread {
-                            activitytext.text = item.activity
-                            participiantstext.text = item.participants.toString()
-                            typetext.text = item.type
-                        }
-
-
-                    } else {
-                        runOnUiThread {
-                            Toast.makeText(this@MainActivity, "Empty response", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-            })
+            fetchData()
         }
+    }
+
+    private fun fetchData(){
+        // Tworzenie klienta OkHttp
+        val client = OkHttpClient()
+
+        // Tworzenie żądania
+        val request = Request.Builder()
+            .url("https://www.boredapi.com/api/activity")
+            .build()
+
+        //Wywołanie żądania asynchronicznie
+        client.newCall(request).enqueue(object : Callback {
+            //Wykona się jeśli zapytanie się nie powiedzie
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Error fetching data", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            //Wykona po odebraniu odpowiedzi od serwera
+            override fun onResponse(call: Call, response: Response) {
+                val responseData = response.body?.string()
+                if (!responseData.isNullOrBlank()) {
+                    //Parsujemy odpowiedż za pomocą biblioteki Gson na obiekt klasy ActivityDto
+                    val gson = Gson()
+                    val item = gson.fromJson(responseData, ActivityDto::class.java)
+
+                    runOnUiThread {
+                        activityText.text = item.activity
+                        participiantsText.text = item.participants.toString()
+                        typeText.text = item.type
+                    }
+
+
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Empty response", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
     }
 }
